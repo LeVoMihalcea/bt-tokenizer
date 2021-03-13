4a25bd5ba5c35b5d35397b2d627761f0ff03f6a4
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-03-13 04:15:52 +0200

    changed base image for dockerfile since it didn't support armv7 architecture

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:11-jre-slim
+FROM openjdk:11.0.5-jdk-slim-buster
 VOLUME /tmp
 COPY target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/leo/bacherlorsthesis/agoratokenserver/AgoraTokenServerApplication.java
+++ src/main/java/leo/bacherlorsthesis/agoratokenserver/AgoraTokenServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgoraTokenServerApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(AgoraTokenServerApplication.class, args);
 	}
