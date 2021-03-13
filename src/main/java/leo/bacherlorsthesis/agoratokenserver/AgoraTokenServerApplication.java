10216644d4ff8cdfa1cd00478d8e7b6ec613e547
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-03-13 03:21:41 +0200

    webhook

--- src/main/java/leo/bacherlorsthesis/agoratokenserver/AgoraTokenServerApplication.java
+++ src/main/java/leo/bacherlorsthesis/agoratokenserver/AgoraTokenServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AgoraTokenServerApplication {
 
-	// testing jenkins builds 3
+	// testing jenkins builds 4
 	public static void main(String[] args) {
 		SpringApplication.run(AgoraTokenServerApplication.class, args);
 	}
