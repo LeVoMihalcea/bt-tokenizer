4ca6c427be8887725816c36d1e0fe73bd056f7da
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-03-13 04:08:44 +0200

    added more priviliges to jenkins user

--- src/main/java/leo/bacherlorsthesis/agoratokenserver/AgoraTokenServerApplication.java
+++ src/main/java/leo/bacherlorsthesis/agoratokenserver/AgoraTokenServerApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgoraTokenServerApplication {
-
-	// testing jenkins builds 6
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AgoraTokenServerApplication.class, args);
 	}
